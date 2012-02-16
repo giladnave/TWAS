9ab224ee2a4b368bc10003ce659fc0caccf671d5
Gilad <gilad@ubuntu-gilad.(none)>
2012-02-16 11:43:53 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TWAS::Application.config.secret_token = 'f6f48084b9c70f5099bf6aca163e8eb34152f5af249fa46f4562368ce1808829f492341c6aa54165b7992454220e1e53dbaec786291fdc447f93ab7e988b9f5c'
