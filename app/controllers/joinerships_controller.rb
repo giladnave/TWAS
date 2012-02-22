e619fa9f98090e2b4043baad07701fdfa712f4b0
Gilad <gilad@ubuntu-gilad.(none)>
2012-02-22 10:58:52 +0200

    joinership controller validations

--- app/controllers/joinerships_controller.rb
+++ app/controllers/joinerships_controller.rb
@@ -5,7 +5,7 @@ class JoinershipsController < ApplicationController
     @bid = Bid.find(params[:id])
 
     js = @bid.joinerships.create(:joiner_id => current_user.id)
-    result = js.errors[:base].first || 'Success'
+    result = js.errors[:base].first || 'You joined the bid successfuly !'
 
     respond_to do |format|
       format.html { redirect_to bid_path(@bid), notice: result }
