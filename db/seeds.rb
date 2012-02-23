fc989ca25f831ca30225b7fdfc6a8bb1255503de
Boris Dinkevich <Boris@dinkevich.com>
2012-02-23 16:34:03 +0200

    Added basic seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+u1 = User.create!(:email => 'u1@email.com', :password => '123213')
+u2 = User.create!(:email => 'u2@email.com', :password => '123213')
+u3 = User.create!(:email => 'u3@email.com', :password => '123213')
+
+b1 = Bid.create(:user => u1, :title => 'Sample bid 1', :join_num => 10)
+b2 = Bid.create(:user => u2, :title => 'Sample bid 2', :join_num => 10)
+b3 = Bid.create(:user => u3, :title => 'Sample bid 3', :join_num => 10)
+
