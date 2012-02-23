45210b43b58efb499d88bc78a118a14bc5bd3fc2
Boris Dinkevich <Boris@dinkevich.com>
2012-02-23 16:34:27 +0200

    Fixed join models on User to include 'joined' and 'participants'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,22 @@ class User < ActiveRecord::Base
   
   has_many :bids, :dependent => :destroy
 
-  has_many :joinerships, :through => :bids
+  has_many :joinerships, 
+    :foreign_key => :joiner_id
 
-  #has_many :joined, 
-    #:through => :joinerships, 
-    #:class_name => :bid, 
-    #:foreign_key => :joiner_id,
-    #:source => :bid
+  has_many :joined, 
+    :through => :joinerships,
+    :source => :bid
+
+  has_many :reverse_joinerships, 
+    :through => :bids, 
+    :class_name => "Joinership", 
+    :source => :joinerships
+
+  has_many :participants, 
+    :through => :reverse_joinerships,
+    :source => :joiner,
+    :class_name => 'User'
   
   accepts_nested_attributes_for :bids
   
